c3d51942cb6c1d8007b7e4a0dc3ba972a9e14803
Monica Liang <monicaliang@rocketmail.com>
2020-12-28 14:28:32 -0500

    completed myBind

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -9,11 +9,33 @@ function sum(...callArgs) {
     return callArgs.reduce((acc, ele) => acc + ele);
 }
 
-
+//this is the bind function.
 Function.prototype.myBind = function (ctx, ...bindArgs) {
     const fn = this;
 
-    return function (...callArgs) {
-        return fn.apply(ctx, bindArgs.concat(callArgs));
-    };
+    // // return function (...callArgs) {
+    //     return fn.apply(ctx, bindArgs.concat(callArgs));
+    // };\
+
+    return fn.apply(ctx, ...bindArgs);
 };
+
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
