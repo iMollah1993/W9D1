73f4c28debec861ced24321431155f5b800834e7
James <james.hansung.park@gmail.com>
2020-12-28 15:23:58 -0500

    W9D1 updated with curry solution

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -1,3 +1,4 @@
+// sum function
 function sum(...callArgs) {
     // let nums = [];
 
@@ -8,6 +9,8 @@ function sum(...callArgs) {
 
     return callArgs.reduce((acc, ele) => acc + ele);
 }
+
+
 
 //this is the bind function.
 Function.prototype.myBind = function (ctx, ...bindArgs) {
@@ -23,20 +26,52 @@ Function.prototype.myBind = function (ctx, ...bindArgs) {
 
 class Cat {
     constructor(name) {
-      this.name = name;
+        this.name = name;
     }
-  
+
     says(sound, person) {
-      console.log(`${this.name} says ${sound} to ${person}!`);
-      return true;
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
     }
-  }
-  
-  class Dog {
+}
+
+class Dog {
     constructor(name) {
-      this.name = name;
+        this.name = name;
     }
-  }
-  
-  const markov = new Cat("Markov");
-  const pavlov = new Dog("Pavlov");
+};
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+
+
+// CurriedSum
+// function sumThree(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+
+// sumThree(4, 20, 6); // == 30
+
+// // you'll write `Function#curry`!
+// let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+// f1 = f1(4); // [Function]
+// f1 = f1(20); // [Function]
+// f1 = f1(6); // = 30
+
+function curriedSum(numArgs) {
+    let numbers = [];
+
+    function _curriedSum(num) { 
+        numbers.push(num);
+        
+        if (numbers.length === numArgs) {
+            console.log(numbers.reduce((acc, num) => acc + num));
+        };
+
+        return _curriedSum
+    };
+
+    return _curriedSum
+};
+
